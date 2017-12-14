fbf293fdfa0742dafcad3611c15126164714536e
Markus Reiter <me@reitermark.us>
2017-12-14 16:18:18 +0100

    Refactoring.

--- assignment-2/energyOfGabor.m
+++ assignment-2/energyOfGabor.m
@@ -1,21 +1,20 @@
 function energy = energyOfGabor(video, gabor_size, sigma, theta)
   [g_even, g_odd] = gaborFilter(gabor_size, sigma, sigma, theta, 2, 0, 0);
-  
+
   frames = [];
-  
+
   video = permute(video, [1, 3, 2]);
-  
+
   for row = 1 : size(video, 3)
     image = squeeze(video(:, :, row));
-        
+
     image_even = conv2(double(image), g_even, 'same');
     image_odd = conv2(double(image), g_odd, 'same');
-    
+
     frame = sqrt(image_even.^2 + image_odd.^2);
-    frame = im2uint8(frame);
-    
-    frames = cat(3, frames, frame); 
+
+    frames = cat(3, frames, frame);
   end
-    
+
   energy = permute(frames, [1, 3, 2]);
 end
--- assignment-2/nineTap.m
+++ assignment-2/nineTap.m
@@ -1,7 +1,7 @@
-function motion = nineTap(video)  
+function motion = nineTap(video)
   f1 = [
     0.0094,  0.1148,  0.3964
-   -0.0601, -0.9213, -0.0601 
+   -0.0601, -0.9213, -0.0601
     0.3964,  0.1148,  0.0094
   ];
   f2 = [
@@ -9,25 +9,24 @@ function motion = nineTap(video)
     0.6383, 1.0,    0.6383
     0.1660, 0.0176, 0.0008
   ];
-  
+
   frames = [];
-  
+
   video = permute(video, [1, 3, 2]);
-          
+
   for row = 1 : size(video, 3)
     image = squeeze(video(:, :, row));
-        
+
     image_1 = conv2(double(image), f1, 'same');
     image_2 = conv2(double(image), f2, 'same');
-    
+
     frame = image_1 .* image_2;
-    frame = im2uint8(frame);
-    
-    frames = cat(3, frames, frame); 
+
+    frames = cat(3, frames, frame);
   end
-  
+
   video = frames;
- 
+
   motion = permute(video, [1, 3, 2]);
 end
 
